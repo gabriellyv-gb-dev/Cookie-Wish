a0c9a75420b1369c71b62ea5c6b650b311d82993
Gabrielly Vitória <gabriellyv1244@gmail.com>
2025-11-21 14:13:19 -0300

    feat: types

---
+++ src/types/components/fortuneCookieProps.ts
@@ -0,0 +1,5 @@
+export interface FortuneCookieProps {
+  isOpen: boolean;
+  onClick: () => void;
+  currentFortune?: string;
+}
---
+++ src/types/components/fortunePaperTypes.ts
@@ -0,0 +1,3 @@
+export interface fortunePaperTypes {
+    fortuneText: string;
+}
